fdace82909505bf4944813e4cf0175d5e516b246
Theo1337 <71170619+Theo1337@users.noreply.github.com>
2022-03-15 23:47:32 -0300

    aad

--- pages/index.js
+++ pages/index.js
@@ -88,10 +88,10 @@ const App = () => {
                 renditionRef.current = rendition
             }}
           />
-          <div style={{ position: 'absolute', bottom: '2.5rem', right: '1rem', left: '1rem', textAlign: 'center', zIndex: 1, backgroundColor: "transparent !important" }}>
+          <div style={{width: "30px", position: 'absolute', bottom: '2.5rem', right: '1rem', left: '1rem', textAlign: 'center', zIndex: 1, backgroundColor: "transparent !important" }}>
             {page}
           </div>
-          <div style={{ position: 'absolute', bottom: '0rem', right: '1rem', left: '1rem', textAlign: 'center', zIndex: 4, backgroundColor: "transparent !important" }}>
+          <div style={{ position: 'absolute', bottom: '0rem', right: '1rem', left: '1rem', textAlign: 'center', zIndex: 1, width: "30px" }}>
             <Button onClick={hidePage}>HIDE</Button>
           </div>
         </div>
