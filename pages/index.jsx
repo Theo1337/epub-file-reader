ba611fbcdd8b25bc050b0876ef3d57e5d534c89c
Theo1337 <bethuel47@gmail.com>
2022-05-03 19:08:46 -0300

    book 2

--- pages/index.jsx
+++ pages/index.jsx
@@ -115,7 +115,7 @@ const App = () => {
         <div style={{ height: "100vh", backgroundColor: "transparent"}}>
           <ReactReader
             locationChanged={locationChanged}
-            url="/assets/book.epub"
+            url="/assets/book2.epub"
             location={location}
             styles={styles}
             getRendition={(rendition) => {
