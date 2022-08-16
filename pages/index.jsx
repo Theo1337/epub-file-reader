6a713041653747cbfcd88aa6dada1a7f08790496
Theo1337 <71170619+Theo1337@users.noreply.github.com>
2022-08-16 09:47:17 -0300

    kkmkn

--- pages/index.jsx
+++ pages/index.jsx
@@ -115,7 +115,7 @@ const App = () => {
         <div style={{ height: "100vh", backgroundColor: "transparent"}}>
           <ReactReader
             locationChanged={locationChanged}
-            url="/assets/sh.epub"
+            url="/assets/01_The_Lightning_Thief.epub"
             location={location}
             styles={styles}
             getRendition={(rendition) => {
