855ee02c206ff83bc5aa740d709f2ec2852f8fe6
Theo1337 <71170619+Theo1337@users.noreply.github.com>
2024-07-25 13:08:50 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -115,7 +115,7 @@ const App = () => {
         <div style={{ height: "100vh", backgroundColor: "transparent"}}>
           <ReactReader
             locationChanged={locationChanged}
-            url="/assets/dxcz.epub"
+            url="/assets/book2.epub"
             location={location}
             styles={styles}
             getRendition={(rendition) => {
