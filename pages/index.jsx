79481967ba15e5e28e7a3966f4ee7079f54ae3ba
Theo <71170619+Theo1337@users.noreply.github.com>
2025-03-28 13:47:27 -0300

    Removed old hide function

--- pages/index.jsx
+++ pages/index.jsx
@@ -49,69 +49,10 @@ const App = () => {
   }
 
   return (
-    <>
+    <div>
       <Head>
         <title>Nova guia</title>
       </Head>
-      {
-      hide ? 
-        <div style={{ backgroundColor: "#323639", width: "100vw", height: "100vh", textAlign: "center",  display: "flex", alignItems: "center", alignContent: "center", minHeight: "100vh", justifyContent: "center"}}>
-          <div style={{ backgroundColor: "transparent", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center"}}>
-            <img style={{ backgroundColor: "transparent" }} src="assets/logo.svg"/>
-            <p />
-            <input 
-              style={{ width: "561px", border: "none",marginTop: "5%", borderRadius: "30px", height: "44px", backgroundColor: "white", outline: "none", fontSize: "16px", padding: "20px"}} 
-              placeholder="Pesquise no Google ou digite um URL"
-              id="icon"
-              disabled={true}
-              >
-              </input>
-            <div style={{ backgroundColor: "transparent", marginTop: "5%", padding: "20px", display: "flex", flexDirection: "row"}}>
-              <div
-              className="box"
-              id="box"
-              onClick={() => {
-                window.location.href = "https://login.plurall.net/"
-              }}
-              style={{ backgroundColor: "transparent", borderRadius: "5px", cursor: "pointer", width: "100px", height: "100%", display: "flex", alignItems: "center", flexDirection: "column", padding: "15px"}}>
-                <div style={{backgroundColor: "#323232", borderRadius: "50%", padding: "10px",  display: "flex", alignItems: "center",}}>
-                  <img style={{ width: "30px", height: "30px", backgroundColor: "transparent" }} src="https://assets.cdn.plurall.net/static/elo/plurall-favicon.png"/>
-                </div>
-                  <div style={{ backgroundColor: "transparent", marginTop: "10px", fontWeight: "light"}}>
-                    Plurall
-                  </div>
-              </div>
-              <div
-              className="box"
-              id="box"
-              onClick={() => {
-                alert(lPage)
-              }}
-              style={{ backgroundColor: "transparent", borderRadius: "5px", cursor: "pointer", width: "100px", height: "100%", display: "flex", alignItems: "center", flexDirection: "column", padding: "15px"}}>
-                <div style={{backgroundColor: "#323232", borderRadius: "50%", padding: "10px",  display: "flex", alignItems: "center",}}>
-                  <img style={{ width: "30px", height: "30px", backgroundColor: "transparent" }} src="/assets/youtube.png"/>
-                </div>
-                  <div style={{ backgroundColor: "transparent", marginTop: "10px", fontWeight: "light"}}>
-                    Youtube
-                  </div>
-              </div>
-              <div
-              className="box"
-              onClick={hidePage}
-              style={{ backgroundColor: "transparent", cursor: "pointer", borderRadius: "5px", width: "100px", height: "100%", display: "flex", alignItems: "center", flexDirection: "column", padding: "15px"}}>
-                <div style={{backgroundColor: "#323232", borderRadius: "50%", padding: "10px",  display: "flex", alignItems: "center", fontWeight: "bolder", fontSize: "20px"}}>
-                <img style={{ width: "30px", height: "30px", backgroundColor: "transparent"}} src="assets/add.svg"/>
-                </div>
-                  <div style={{ backgroundColor: "transparent", marginTop: "10px", fontWeight: "light", whiteSpace: "nowrap" }}>
-                    Novo atalho
-                  </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      
-      :
-      
         <div style={{ height: "100vh", backgroundColor: "transparent"}}>
           <ReactReader
             locationChanged={locationChanged}
@@ -144,8 +85,7 @@ const App = () => {
             <div>Last page: {lPage}</div>
           </div>
         </div>
-      }
-    </>
+    </div>
   )
 }
 
